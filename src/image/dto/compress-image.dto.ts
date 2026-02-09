a99a5607499ad4dd078871c6d1cc0fb14efacecb
jnaugusto <>
2026-02-09 23:55:04 +0800

    feat: added swagger and an http file document to test compress image

--- src/image/dto/compress-image.dto.ts
+++ src/image/dto/compress-image.dto.ts
@@ -1,7 +1,15 @@
-import { IsOptional, IsInt, Min, Max } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class CompressImageDto {
+  @ApiProperty({
+    description: 'The quality of the compressed image (1-100)',
+    default: 80,
+    required: false,
+    minimum: 1,
+    maximum: 100,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import '@total-typescript/ts-reset';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
@@ -8,6 +9,15 @@ import { env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Jess Server API')
+    .setDescription('The Jess Server API description')
+    .setVersion('1.0')
+    .addTag('jess')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   const httpAdapterHost = app.get(HttpAdapterHost);
 
