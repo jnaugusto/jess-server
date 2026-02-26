a29bd6658af6d62843d1418504c58e6b04c4b8e8
jnaugusto <>
2026-02-27 02:05:57 +0800

    fix: cleanup

--- test-auth.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const auth: import("better-auth", { with: { "resolution-mode": "import" } }).Auth<{
-    database: {
-        dialect: string;
-        type: string;
-    };
-    emailAndPassword: {
-        enabled: true;
-    };
-}>;
